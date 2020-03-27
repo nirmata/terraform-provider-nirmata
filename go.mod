f4154aeafe2b0135f388f901a3afca60178fb647
Yuvraj <yuvraj.yad001@gmail.com>
2020-03-27 22:43:00 +0530

    nirmata error package added

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/nirmata/terraform-provider-nirmata
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.25.3
 	github.com/hashicorp/terraform v0.12.24
 	github.com/hashicorp/terraform-plugin-sdk v1.9.0
 	github.com/nirmata/go-client v0.0.0-20200327050957-89e1a1082866
+	go.tmthrgd.dev/gomodpriv v0.0.0-20191024122841-38d17a72f03c // indirect
 )
---
+++ pkg/nirmata/nirmataerr.go
@@ -0,0 +1,74 @@
+package nirmata
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/nirmata/go-client/nirmata/nirmataerr"
+)
+
+// Returns true if the error matches all these conditions:
+//  * err is of type nirmataerr.Error
+//  * Error.Code() matches code
+//  * Error.Message() contains message
+func isNirmataErr(err error, code string, message string) bool {
+	var nirmataErr nirmataerr.Error
+	if errors.As(err, &nirmataErr) {
+		return nirmataErr.Code() == code && strings.Contains(nirmataErr.Message(), message)
+	}
+	return false
+}
+
+// Returns true if the error matches all these conditions:
+//  * err is of type nirmataerr.RequestFailure
+//  * RequestFailure.StatusCode() matches status code
+// that sometimes only respond with status codes.
+func isNirmataErrRequestFailureStatusCode(err error, statusCode int) bool {
+	var nirmataErr nirmataerr.RequestFailure
+	if errors.As(err, &nirmataErr) {
+		return nirmataErr.StatusCode() == statusCode
+	}
+	return false
+}
+
+func retryOnNirmataCode(code string, f func() (interface{}, error)) (interface{}, error) {
+	var resp interface{}
+	err := resource.Retry(2*time.Minute, func() *resource.RetryError {
+		var err error
+		resp, err = f()
+		if err != nil {
+			nirmataErr, ok := err.(nirmataerr.Error)
+			if ok && nirmataErr.Code() == code {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	return resp, err
+}
+
+// RetryOnNirmataCodes retries Nirmata error codes for one minute
+// Note: This function will be moved out of the nirmata package in the future.
+func RetryOnNirmataCodes(codes []string, f func() (interface{}, error)) (interface{}, error) {
+	var resp interface{}
+	err := resource.Retry(1*time.Minute, func() *resource.RetryError {
+		var err error
+		resp, err = f()
+		if err != nil {
+			nirmataErr, ok := err.(nirmataerr.Error)
+			if ok {
+				for _, code := range codes {
+					if nirmataErr.Code() == code {
+						return resource.RetryableError(err)
+					}
+				}
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	return resp, err
+}
