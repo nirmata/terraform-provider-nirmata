5371498866f99406f6d3254d5c7dcefb59503aaf
Yuvraj <yuvraj.yad001@gmail.com>
2020-03-29 14:33:56 +0530

    Added cluster GKE resources

--- pkg/nirmata/provider.go
+++ pkg/nirmata/provider.go
@@ -28,6 +28,7 @@ func Provider() *schema.Provider {
 
 		ResourcesMap: map[string]*schema.Resource{
 			"nirmata_host_group_direct_connect": resourceHostGroupDirectConnect(),
+			"nirmata_cluster_gke":               resourceClusterGke(),
 		},
 
 		ConfigureFunc: configureProvider,
