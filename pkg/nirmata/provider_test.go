de379e26012175214c42fbf530797240dc5958a2
Jim Bugwadia <jim@nirmata.com>
2020-03-28 22:10:51 -0700

    - implement Direct Connect HG create / delete

--- pkg/nirmata/provider_test.go
+++ pkg/nirmata/provider_test.go
@@ -2,28 +2,10 @@ package nirmata
 
 import (
 	"testing"
-
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
 )
 
-var testAccProvider *schema.Provider
-
-func init() {
-	testProvider = Provider().(*schema.Provider)
-}
-
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
-}
-
-func TestProvider_impl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
-}
-
-func testPreCheck(t *testing.T) {
-	// We will use this function later on to make sure our test environment is valid.
-	// For example, you can make sure here that some environment variables are set.
 }
