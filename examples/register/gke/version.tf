aaa425dd359870827db4ccd61ded88ebc32e8d84
vyankatesh <vyankatesh@neualto.com>
2021-03-08 23:24:48 +0530

    change kubectl version

--- examples/register/gke/version.tf
+++ examples/register/gke/version.tf
@@ -1,8 +1,14 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    nirmata = {
-      source  = "local/nirmata/nirmata"
+      nirmata = {
+      source  = "registry.terraform.io/nirmata/nirmata"
+      version = "1.0.0"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
 }
