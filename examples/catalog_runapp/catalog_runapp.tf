fb74692d5160ab7d155b2afac22b2de5371b583f
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2021-09-10 14:14:19 +0530

    remove unwanted code

--- examples/catalog_runapp/catalog_runapp.tf
+++ examples/catalog_runapp/catalog_runapp.tf
@@ -1,19 +1,19 @@
 provider "nirmata" {
   #  Nirmata API Key. Best configured as the environment variable NIRMATA_TOKEN.
   
-   token = "L0DIoi44ma1FFDjNVqEtdZaZTprTlYDrxbDvY1CiElSiFLbaVBySRth/aiZcGS25BZpKf3kI6Cee3lYvEhcMaA=="
+  #  token = ""
 
   #  Nirmata address. Defaults to https://nirmata.io and can be configured as
   #  the environment variable NIRMATA_URL.
   
-   url = "https://devtest5.nirmata.co/"
+  #  url = ""
 }
 
 resource "nirmata_run_application" "tf-catalog-run-app" {
-  name                = "tf-run-app-new"
-  application         = "test-3"
-  catalog             = "default-policy-catalog"
-  version             = "auto-0.0.1-6f28035"
+  name                = "tf-run-app"
+  application         = ""
+  catalog             = ""
+  version             = ""
   channel             = "Rapid"
-  environments        = ["tf-env", "tf-env-1"]
+  environments        = []
  }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/google/uuid v1.1.1
 	github.com/hashicorp/terraform-plugin-sdk v1.9.0
 	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af
